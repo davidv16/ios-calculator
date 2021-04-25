766ca4f5ab7e396719922c5c385d1f752464c90f
Davíð Valdimar Arnalds <david@Davis-iMac-2.hringdu.is>
2021-04-25 18:42:09 +0000

    some commenting in the button section

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,6 +5,9 @@ export default function Button(props) {
   return (
     <View>
       {/* add styling from StyleSheet and pass background colors from HomeScreen */}
+      {/* styles added from StyleSheet and text colors passed by props from HomeScreen */}
+      {/* checks if button being used is the Large Zero one and renders
+      different styling if so.  */}
       <TouchableHighlight
         style={[
           props.largeButton ? styles.largeButton : styles.button,
@@ -35,6 +38,8 @@ export default function Button(props) {
 }
 
 {/* Stylesheet for the buttons */}
+{/* Size and radius styling for buttons and button text */}
+{/* Special styling for the large Zero button and text */}
 const styles = StyleSheet.create({
   button: {
     height: 76,
